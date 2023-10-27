5b3ff0a8fde56ec5712585dbc8ec567b6b078ee6
divinedanny <divinedaniel202@gmail.com>
2023-10-27 16:04:18 +0100

    learning the basis of react and how the process of the react is returned as a html element

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Youtube from './Youtube'
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank',
+//     },
+//     children: 'Click me!'
+// }
+
+// function MyApp(){
+//     return(
+//         <div>
+//             <h1>My App</h1>
+//         </div>
+//     )
+// }
+
+// const AnotherElement = (
+//     <a href="https://www.google.com" target="_blank">
+//         Visit Google
+//     </a>
+// )
+
+// const areactElement = React.createElement(
+//     'a',
+//     {
+//         href: "https://www/joinpopupnow.com",
+//         target: "_blank"
+//     },
+//     'Join Popup Now'
+// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    // <App />
+    // <Youtube />
+    // <MyApp />
+    // areactElement
+    // AnotherElement
     <App />
-  </React.StrictMode>,
 )
